6e0a584afdf2aa5212266640a5b9e90439c6d0ef
sanjakgPURS <sanjak79@hotmail.com>
2016-12-14 10:29:41 +0100

    Drugo komitovanje srijeda

--- SanjaKG16 - Parametri metoda/src/App.java
+++ SanjaKG16 - Parametri metoda/src/App.java
@@ -1,10 +1,16 @@
 class Zaba {
 	private String ime;
 	private int godine;
+	private String boja;
 	
 	public void setName(String name){
 		this.ime=name;
 	};
+	
+	public void setBoja(String boja){
+		this.boja=boja;
+	};
+	
 	
 	public void setAge(int age){
 		this.godine=age;
@@ -18,10 +24,17 @@ class Zaba {
 		return godine;
 	}
 	
-	public void setSve(String name, int age){
+	public void setSve(String name,String boja, int age){
 		setName(name);
+		setBoja(boja);
 		setAge(age);
+		
 	}
+	
+	
+	public String VratiZvuk(){
+		return "Kre kre";
+	};
 	
 		
 }
@@ -35,10 +48,13 @@ public class App {
 		// TODO Auto-generated method stub
 
 	Zaba zabica=new Zaba();
-	zabica.setSve("Kreketusa", 2);
+	zabica.setSve("Kreketusa","zelena", 2);
 	
-	System.out.println(zabica.getName()+" stara "+ zabica.getAge()+" godine ");
-		
+	Zaba zabuljak=new Zaba();
+	zabuljak.setSve("Kreketavica","siva", 3);
+	
+	System.out.println(zabica.getName()+" stara "+ zabica.getAge()+" godine, kaže "+ zabica.VratiZvuk());
+	System.out.println(zabuljak.getName()+" stara "+ zabuljak.getAge()+" godine, kaže "+ zabuljak.VratiZvuk());	
 		
 		
 	}
